03beb247a8af13cff702a298e1bf61fb0d4e9e26
eugeniofciuvasile <e.vasile@it.digi.eu>
2025-11-15 01:38:04 +0100

    remove centeredbox

--- internal/ui/components/connection_list.go
+++ internal/ui/components/connection_list.go
@@ -12,11 +12,9 @@ import (
 )
 
 var (
-	titleStyle        = lipgloss.NewStyle().MarginLeft(2)
-	itemStyle         = lipgloss.NewStyle().PaddingLeft(4)
-	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("170"))
-	paginationStyle   = list.DefaultStyles().PaginationStyle.PaddingLeft(4)
-	helpStyle         = list.DefaultStyles().HelpStyle.PaddingLeft(4).PaddingBottom(1)
+	titleStyle      = lipgloss.NewStyle().MarginLeft(2)
+	paginationStyle = list.DefaultStyles().PaginationStyle.PaddingLeft(4)
+	helpStyle       = list.DefaultStyles().HelpStyle.PaddingLeft(4).PaddingBottom(1)
 )
 
 type ToggleOpenInNewTerminalMsg struct{}
